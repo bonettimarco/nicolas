df79dcf4a2034eb6b4a9b92267e8c705f5484b09
Marco <marco_bonetti@uol.com.br>
2015-07-23 10:33:58 -0300

    -all

---
+++ java.js
@@ -0,0 +1,5 @@
+var quadrado=function (num){
+	return num*num;
+}
+var numero = prompt("digite um número");
+console.log(quadrado(numero));
---
+++ script.js
@@ -0,0 +1,13 @@
+var quadrado=function (num){
+	return num*num;
+}
+
+var numero = prompt("digite um número");
+console.log(quadrado(numero));
+
+
+if (numero){
+		console.log("Tem número");
+}else{
+		console.log(a);
+}
